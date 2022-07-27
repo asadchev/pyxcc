23c42b06214652146722c5435ba352e828037995
asadchev <asadchev@gmail.com>
2022-07-27 16:59:44 -0400

    cartesian.py

--- pyxcc/cartesian.py
+++ pyxcc/cartesian.py
@@ -1,4 +1,4 @@
-import itertools,numpy
+import itertools,math,numpy
 
 def nbf(L):
   return ((L+1)*(L+2))//2
@@ -10,12 +10,21 @@ class Orbital(tuple):
     return Orbital(*(a+b for a,b in zip(self,other)))
   def __sub__(self,other):
     return Orbital(*(a-b for a,b in zip(self,other)))
+  def __ge__(self,other):
+    return all(a>=b for a,b in zip(self,other))
+  def comb(self,k):
+    return math.prod(math.comb(a,b) for a,b in zip(self,k))
   @property
   def L(self): return sum(self)
   @property
   def array(self): return numpy.array(self)
   @property
   def nonzero(self): return tuple(i for i,v in enumerate(self) if v)
+  @property
+  def index(self):
+    i,j,k = self
+    jk = j+k;
+    return (jk*(jk+1))//2 + k
 
 def axis(j):
   return Orbital(*(int(i==j) for i in range(3)))
@@ -29,3 +38,12 @@ def orbitals(*args):
         j = l-k
         f.append(Orbital(i,j,k))
   return tuple(f)
+
+def index(A):
+  return Orbital(*A).index
+
+def index0(A):
+  i,j,k = A
+  L = i+j+k
+  first = (L*(L+1)*(L+2))//6
+  return first + index(A)

7868cfa7fb0dfc14a4368b97a3d9756b4a4a99a1
asadchev <asadchev@gmail.com>
2022-07-27 16:59:51 -0400

    hrr

--- pyxcc/gto/reference/hrr.py
+++ pyxcc/gto/reference/hrr.py
@@ -27,11 +27,61 @@ class HGP:
     return H
 
   @staticmethod
-  def hrr(bra,ket,I):
-    if bra and bra.second:
-      I = hrr1(bra.first, bra.second, I)
-    if ket and ket.second:
-      I = numpy.moveaxis(I,[-1],0)
-      I = hrr1(ket.first, ket.second, I)
-      I = hrr(numpy.moveaxis(I,[0,1],-1))
+  def hrr(A,B,R,G):
+    from pyxcc.cartesian import orbitals,axis
+    #print (G.shape)
+    for t in range(0,B):
+      As = orbitals(*range(A,A+B-t+1))
+      At = orbitals(*range(A,A+B-t))
+      Bs = orbitals(t)
+      Bt = orbitals(t+1)
+      H = HGP.hrr1(As,Bs,At,Bt,R,G)
+      G = H
+    return H
+
+
+class Direct:
+
+  @staticmethod
+  def hrr(A,B,R,G):
+    from pyxcc.cartesian import nbf,orbitals,index0
+    from numpy import prod
+    index = lambda t: index0(t) - index0([A,0,0])
+
+    As = []
+    for L in range(0,B+1):
+      for t in orbitals(L):
+        idx = [index(a) for a in orbitals(A+L) if a >= t]
+        As.append(G[idx,0])
+
+    #print (As)
+
+    R = numpy.array(R)
+    I = numpy.zeros([nbf(A),nbf(B)])
+
+    R_ = R
+    R = lambda i,j,k: prod(R_**(i,j,k))
+
+    # if B == 1:
+    #   I[:,0] = R(1,0,0)*As[0] + As[1]
+    #   I[:,1] = R(0,1,0)*As[0] + As[2]
+    #   I[:,2] = R(0,0,1)*As[0] + As[3]
+    #   return I
+
+    # if B == 2:
+    #   I[:,0] = R(2,0,0)*As[0] + R(1,0,0)*As[1] + R(1,0,0)*As[1] + As[4]
+    #   I[:,1] = R(1,1,0)*As[0] + R(1,0,0)*As[2] + R(0,1,0)*As[1] + As[5]
+    #   I[:,2] = R(1,0,1)*As[0] + R(1,0,0)*As[3] + R(0,0,1)*As[1] + As[6]
+    #   I[:,3] = R(0,2,0)*As[0] + R(0,1,0)*As[2] + R(0,1,0)*As[2] + As[7]
+    #   I[:,4] = R(0,1,1)*As[0] + R(0,1,0)*As[3] + R(0,0,1)*As[2] + As[8]
+    #   I[:,5] = R(0,0,2)*As[0] + R(0,0,1)*As[3] + R(0,0,1)*As[3] + As[9]
+    #   return I
+
+    for L in range(0,B+1):
+      for t in orbitals(L):
+        for x in orbitals(B-L):
+          #print ("b=",x+t,(x+t).index,"=",'x',x,'t',t,'@',index0(t))
+          M = (x+t).comb(t)
+          I[:,(x+t).index] += M*R(*x)*As[index0(t)]
+
     return I
--- tests/gto/test_hrr.py
+++ tests/gto/test_hrr.py
@@ -18,16 +18,45 @@ def test_hrr_hgp():
     1
   )
 
+  H = G
   for t in range(0,B):
     As = orbitals(*range(A,A+B-t+1))
     At = orbitals(*range(A,A+B-t))
     Bs = orbitals(t)
     Bt = orbitals(t+1)
-    H = HGP.hrr1(As,Bs,At,Bt,R,G)
+    I = HGP.hrr1(As,Bs,At,Bt,R,H)
     for k in (0,1,2):
       for ia,a in enumerate(At):
         for ib,b in enumerate(Bs):
           ia_plus_1 = As.index(a + axis(k))
           ib_plus_1 = Bt.index(b + axis(k))
-          assert(H[ia,ib_plus_1] == approx(R[k]*G[ia,ib] + G[ia_plus_1,ib]))
-    G = H
+          assert(I[ia,ib_plus_1] == approx(R[k]*H[ia,ib] + H[ia_plus_1,ib]))
+    H = I
+
+  assert(np.allclose(H, HGP.hrr(A,B,R,G)))
+
+
+def test_hrr_direct():
+
+  print()
+
+  Direct = hrr.Direct
+  HGP = hrr.HGP
+
+  A = 4
+  B = 4
+
+  R = (3.14, -2.71, 0.5)
+
+  G = np.random.rand(
+    sum(nbf(L) for L in range(A,A+B+1)),
+    1
+  )
+
+  H = Direct.hrr(A,B,R,G)
+  Ref = HGP.hrr(A,B,R,G)
+
+  # print(H)
+  # print(Ref)
+
+  assert(np.allclose(Direct.hrr(A,B,R,G), Ref))
